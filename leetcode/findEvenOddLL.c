b850f499c4259b4651769303fbb8e6eae8011c5e
hadigal <hadigal2992@gmail.com>
2019-03-03 19:54:16 -0800

    adding o(1)  and o(log n) solution for prob 348

--- leetcode/findEvenOddLL.c
+++ leetcode/findEvenOddLL.c
@@ -11,85 +11,108 @@
 
 // }
 
-struct ListNode *create(int val)
-{
-    struct ListNode *node = (struct ListNode *)calloc(1,sizeof(*node));
-    node->val = val;
-    node->next = NULL;
-    return node;
-}
+// struct ListNode *create(int val)
+// {
+//     struct ListNode *node = (struct ListNode *)calloc(1,sizeof(*node));
+//     node->val = val;
+//     node->next = NULL;
+//     return node;
+// }
 
 struct ListNode* oddEvenList(struct ListNode* head)
 {
-    if(head == NULL || head->next == NULL || head->next->next == NULL)
+    if(head == NULL || head->next == NULL)
     {
         return head;
     }
 
-    struct ListNode *temp;
+
+    //struct ListNode *temp;
 
     struct ListNode *even = head->next;;
     struct ListNode *odd = head;
 
-    struct ListNode *evenStart = NULL;
-    struct ListNode *oddStart = NULL;
+    struct ListNode *evenStart = even;
+    struct ListNode *oddStart = odd;
 
-    struct ListNode *evenEndPtr = NULL;
-    struct ListNode *oddEndPtr = NULL;
-
-    int count = 1;
-
-    while(even != NULL && odd != NULL)
+    while(odd->next != NULL && even->next != NULL)
     {
-        if((count % 2) != 0)
-        {
-            temp = create(odd->val);
-            if(oddStart != NULL)
-            {
-                oddEndPtr->next = temp;
-                oddEndPtr = oddEndPtr->next;
-            }
-            else if(oddStart == NULL)
-            {
-                oddStart = temp;
-                oddEndPtr = oddStart;
-            }
+      odd->next = even->next;
+      even->next = odd->next->next;
 
-            ++count;
-        }
-
-        if(count%2 == 0)
-        {
-            temp = create(even->val);
-            if(evenStart != NULL)
-            {
-                evenEndPtr->next = temp;
-                evenEndPtr = evenEndPtr->next;
-            }
-            else if(evenStart == NULL)
-            {
-                evenStart = temp;
-                evenEndPtr = evenStart;
-            }
-        }
-
-        ++count;
-        odd = even->next;
-        if(odd != NULL)
-        {
-            even = odd->next;
-        }
+      odd = odd->next;
+      even = even->next;
     }
 
-    if(even == NULL && odd != NULL)
-    {
-        temp = create(odd->val);
-        oddEndPtr->next = temp;
-        oddEndPtr = oddEndPtr->next;
-    }
-
-
-    oddEndPtr->next = evenStart;
-
+    odd->next = evenStart;
     return oddStart;
+
+
+
+    // struct ListNode *temp;
+    //
+    // struct ListNode *even = head->next;;
+    // struct ListNode *odd = head;
+    //
+    // struct ListNode *evenStart = NULL;
+    // struct ListNode *oddStart = NULL;
+    //
+    // struct ListNode *evenEndPtr = NULL;
+    // struct ListNode *oddEndPtr = NULL;
+    //
+    // int count = 1;
+    //
+    // while(even != NULL && odd != NULL)
+    // {
+    //     if((count % 2) != 0)
+    //     {
+    //         temp = create(odd->val);
+    //         if(oddStart != NULL)
+    //         {
+    //             oddEndPtr->next = temp;
+    //             oddEndPtr = oddEndPtr->next;
+    //         }
+    //         else if(oddStart == NULL)
+    //         {
+    //             oddStart = temp;
+    //             oddEndPtr = oddStart;
+    //         }
+    //
+    //         ++count;
+    //     }
+    //
+    //     if(count%2 == 0)
+    //     {
+    //         temp = create(even->val);
+    //         if(evenStart != NULL)
+    //         {
+    //             evenEndPtr->next = temp;
+    //             evenEndPtr = evenEndPtr->next;
+    //         }
+    //         else if(evenStart == NULL)
+    //         {
+    //             evenStart = temp;
+    //             evenEndPtr = evenStart;
+    //         }
+    //     }
+    //
+    //     ++count;
+    //     odd = even->next;
+    //     if(odd != NULL)
+    //     {
+    //         even = odd->next;
+    //     }
+    // }
+    //
+    // if(even == NULL && odd != NULL)
+    // {
+    //     temp = create(odd->val);
+    //     oddEndPtr->next = temp;
+    //     oddEndPtr = oddEndPtr->next;
+    // }
+    //
+    //
+    // oddEndPtr->next = evenStart;
+    //
+    // return oddStart;
 }
